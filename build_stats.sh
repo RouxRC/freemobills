b3d1b3311f3d5dfc7078d71c6af30f86d9323dfd
RouxRC <b.ooghe@gmail.com>
2018-03-18 23:52:06 +0100

    adapt to new bills format

--- build_stats.sh
+++ build_stats.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-mkdir data
+mkdir -p data
 
 echo "mois,sms" > data/stats_sms.csv
 ls factures/*.pdf | while read f; do
   sms=$(pdftotext -raw "$f" -                |
          tr '\n' ' '                         |
-         sed 's/\( [A-Z][a-z]\)/\n\1/g'      |
-         grep Envois                         |
+         sed 's/France/france/g'             |
+         sed 's/\( [A-Z][A-Za-z]\)/\n\1/g'   |
+         grep "\(Envois\|MS depuis\)"        |
          sed 's/^[^0-9]\+//'                 |
          sed 's/[^0-9].*$//'                 |
          awk '{s+=$1} END {print s}')
